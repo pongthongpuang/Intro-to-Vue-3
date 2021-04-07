18fdf7c806188d64b397760123f4a1020b0f1e65
pongthongpuang <6206021620108@fitm.kmutnb.ac.th>
2021-04-07 17:24:40 +0700

    test

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -5,9 +5,9 @@ app.component('product-display', {
       required: true
     }
   },
-  template: 
-  /*html*/
-  `<div class="product-display">
+  template:
+    /*html*/
+    `<div class="product-display">
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
@@ -54,44 +54,44 @@ app.component('product-display', {
   </div>`,
   data() {
     return {
-        product: 'Socks',
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        details: ['50% cotton', '30% wool', '20% polyester'],
-        variants: [
-          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+      ]
     }
   },
   methods: {
-      addToCart() {
-          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
-      },
-      // solution
-      removeFromCart() {
-        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
-      },
-      // solution
-      updateVariant(index) {
-          this.selectedVariant = index
-      }
+    addToCart() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+    },
+    // solution
+    removeFromCart() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+    },
+    // solution
+    updateVariant(index) {
+      this.selectedVariant = index
+    }
   },
   computed: {
-      title() {
-          return this.brand + ' ' + this.product
-      },
-      image() {
-          return this.variants[this.selectedVariant].image
-      },
-      inStock() {
-          return this.variants[this.selectedVariant].quantity
-      },
-      shipping() {
-        if (this.premium) {
-          return 'Free'
-        }
-        return 2.99
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+    shipping() {
+      if (this.premium) {
+        return 'Free'
       }
+      return 2.99
+    }
   }
 })
